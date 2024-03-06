35da3f2a09b7267869b0e160df3a54fe1642f892
arunpragash-periyasamy <arunpragash46@gmail.com>
2024-03-06 15:34:38 +0530

    Added the env file and installed dotenv package

--- server.js
+++ server.js
@@ -2,10 +2,13 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
+require('dotenv').config();
 const authentication = require('./routes/authentication');
 const mongoose = require('mongoose');
-
-const mongoose_URI = "mongodb://localhost:27017/ecommerce";
+app.use(bodyParser.json());
+app.use(cors({origin:"*"}))
+app.use("/authentication",authentication);
+const mongoose_URI = process.env.MONGO_URI;
 
 mongoose.connect(mongoose_URI,{ useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
@@ -16,22 +19,6 @@ db.once('open', function() {
     console.log('Connected to MongoDB using Mongoose successfully');
 });
 
-
-
-app.use(cors({ origin: '*' }))
-app.use(bodyParser.json());
-app.use("/authentication",authentication);
-
-
-
-
-
-
-
-
-
-
-
-
-
-app.listen(3000)
+app.listen(process.env.PORT,()=>{
+    console.log("Server is running on port " + process.env.PORT);
+})
